77e90a5efdc5f0209cea2afd1f95ada6f89400c3
Kevin Ganan <kganan0448@uta.edu.ec>
2022-07-16 21:01:24 -0500

    Se agrego las gastronomías de los cantones de Tungurahua

--- turismo_tungurahua/views/interfaces/contactanos.php
+++ turismo_tungurahua/views/interfaces/contactanos.php
@@ -1,1 +1,125 @@
-estas en Gastronomia
+<html>
+<head>
+</head>
+<style>
+#Caratula{
+    background-color: #F29F05;
+    font-family: Arial, Helvetica, sans-serif;
+    background-image: url(pictures/volcan.jpg);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 80%;
+    height: 400px;
+    padding: 0px;
+    
+}
+#Introduccion{
+    font-size: 1.4em;
+    position: absolute;
+        left: 60%;
+       background-size: cover;
+       background-color: #FFFFFF;
+       opacity: .4;
+       height: 400px;
+       padding: 30px;
+       }
+#uno{
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1.9em;
+       height: 300px;
+       padding: 20px;
+       }
+.uno_img{
+    padding: 20px;
+    width: 45%;
+    float: left;
+    margin-right: 1em;
+}
+.dos_img{
+    padding: 20px;
+    width: 40%;
+    float: left;
+    margin-right: 1em;
+}
+</style> 
+<body>
+<div id="Caratula" >   
+    <img src="pictures/tungurahua.jpg" class="uno_img">
+    <div id="Introduccion">
+    <table>
+    <h2>GASTRONOMÍA</h2>
+<tr><td align="justify">Tungurahua tiene una gran tradición gastronómica. Cada uno de sus cantones se caracteriza
+    por sus delicias y especialidades, así tenemos: en Patate la elaboración de arepas, chicha
+    de uva y vinos, en Ambato es típico el caldo de 31, los chinchulines, quimbolitos, las 
+    tortillas con chorizo, el llapingacho ambateño, y el caldo de librillo, entre otras, en Pinllo
+    lo más característico es su pan y las gallinas asadas, en Cevallos se especializan en la fritada, 
+    yahuarlocro, caldo de patas, caldo de gallina, entre otros, en Mocha es característico los cuyes
+    y conejos asados, el caldo de gallina criolla, las papas con cuero, las habas con queso y el 
+    yahuarlocro, además de los famoso dulces elaborados a base de caña como las melcochas, los 
+    dulces de guayaba y muchas otras golosinas.</td></tr>
+</table> 
+    <p>
+    </p> 
+    </div>
+</div>
+
+<div id="Caratula" >   
+    <img src="pictures/ambato1.jpg" class="uno_img">
+    <div id="Introduccion">
+    <table>
+    <h2>AMBATO</h2>
+<tr><td align="justify">Ambato es una tierra con una arquitectura muy variada, una característica que la hace única son sus manifestaciones culturales populares que tienen, su máxima expresión en el Carnaval se da con la Fiesta de las Flores y las Frutas. También se caracteriza por su amplia gastronomía, aqui se presentan algunos platos tipocos de la Ciudad de Ambato.<li>Llapingachos</li>
+<li>Pan de Pinllo</li>
+<li>Colada Morada y Empanadas de Viento</li>
+<li>Gallinas de Pinllo</li>
+<li>Cuyes de Ficoa</li></td></tr>
+</table> 
+    </div>
+</div>
+
+<div id="Caratula" >   
+    <img src="pictures/baños.jpg" class="uno_img">
+    <div id="Introduccion">
+    <table>
+    <h2>BANOS</h2>
+<tr><td align="justify">Deliciosos platos típicos que podrás disfrutar en el Cantón Baños de Agua Santa. Es un deleite para los turistas, encontrar en un solo hermoso país la sazón de cuatro regiones es una locura que solo los ecuatorianos lo pueden hacer posible. A continuación se presentan algunos productos tradicionales de este lugar: <li>Melcochas y Dulces</li>
+<li>Jugo de Caña</li>
+<li>Sánduche - Cóctel</li>
+<li>Camotes en Caldo de Carne</li>
+<li>Caldo de Gallina - Licor</li></td></tr>
+</table> 
+    </div>
+</div>
+
+<div id="Caratula" >   
+    <img src="pictures/patate1.jpg" class="uno_img">
+    <div id="Introduccion">
+    <table>
+    <h2>PELILEO</h2>
+<tr><td align="justify">Existen tradiciones y costumbres en la sociedad y en la vida, que unas desaparecen, otras en cambio progresan. Con la anuencia de la familia Castro, me permito en pocas lineas, resumir el origen y el servicio de esta tradicional gastronomía como se presenta: 
+<li>Tamales</li>
+<li>Fritada</li>
+<li>Empanadas de tiesto</li>
+<li>Cuyes</li>
+<li>Hornado</li>
+</table> 
+    </div>
+</div>
+
+<div id="Caratula" >   
+    <img src="pictures/pelileo1.jpg" class="uno_img">
+    <div id="Introduccion">
+    <table>
+    <h2>PATATE</h2>
+<tr><td align="justify">Una de las virtudes de Patate, es su tradicional comida típica que se encuentra con facilidad recorriendo las calles del cantón. La deliciosa gastronomía que ofrecen sus habitantes se compone de ingredientes como el zapallo y variedad de deliciosas frutas. Entre las más
+conocidas están la deliciosa trucha a la mandarina, arepas, vino de uvas
+de la zona, chicha, espumilla y más.
+<li>Vino de uva</li>
+<li>Chicha de Uva</li>
+<li>Arepas de Zapallo</li>
+</table> 
+    </div>
+</div>
+
+    </body>
+</html>
