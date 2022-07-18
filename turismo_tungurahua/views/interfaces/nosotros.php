30abb68c2cd9687acf2d2e616ff9a25a486f5039
mou <mvillafuerte0383@uta.edu.ec>
2022-07-17 21:08:09 -0500

    copilacion

--- turismo_tungurahua/views/interfaces/nosotros.php
+++ turismo_tungurahua/views/interfaces/nosotros.php
@@ -1,1 +1,121 @@
-Estas en Fiestas
+<html>
+<head>
+</head>
+<style>
+h1 {
+    font-family: Arial, Helvetica, sans-serif;
+    text-align: center;
+    font-size: 1.4em;
+}
+h2{
+    font-family: Arial, Helvetica, sans-serif;
+}
+ul{
+    font-family: Arial, Helvetica, sans-serif;
+
+}
+p{
+    font-family: Arial, Helvetica, sans-serif;
+    margin-top: 40px;
+    font-size: 1.1em;   
+}
+div{
+    margin-left: 10%;
+    margin-right: 10%;
+    height: 400px;
+}
+a { text-decoration: none; 
+    color: black;}
+
+#Indice{
+    padding: 20px;
+    height: 200px;
+}
+#intro{  
+    padding: 20px;
+    height: 60px; 
+}
+#intro_text{  
+    font-family: Arial, Helvetica, sans-serif;
+    margin-top: 5px;
+    font-size: 1.1em;   
+}
+.uno_img{
+    padding: 20px;
+    width: 40%;
+    float: left;
+    margin-right: 1em;
+}
+.dos_img{
+    padding: 20px;
+    width: 40%;
+    float: right;
+    margin-right: 1em;
+}
+</style> 
+<body>
+
+<h1>Fiestas Tradicionales de Tungurahua</h1>
+<div id="intro">
+<p id="intro_text">
+La provincia de la sierra central Tungurahua guarda sigilosamente costumbre y 
+tradiciones relevantes para realizar las festvidades. Conoce las fiestas más 
+importantes que tiene Tungurahua en las 9 rutas agroturísticas.
+</p>
+</div>
+<div id="Indice">
+<ul><b>Fiestas:</b></ul>
+<ul><a href="#Frutas_Flores">Fiesta de la fruta y de las flores</a></ul>
+<ul><a href="#Sol_Noviembre">Sol de Noviembre</a></ul>
+<ul><a href="#diablada">La diablada pillareña</a></ul>
+<ul><a href="#Señor_Terremoto">Señor del Terremoto de Patate</a></ul>
+</div>
+
+<div id="uno">
+<h2 id="Frutas_Flores">Fiesta de las flores y las frutas en Ambato</h2>
+<img src="pictures/carnabal.jpg" class="uno_img">
+<p>
+Esta fiesta popular es la más importante para sus pobladores. 
+Se lleva a cabo entre los meses de febrero y marzo. Tiene cuatro 
+números principales alrededor de los cuales gira toda la celebración: 
+El Pregón y la elección de la Reina, la Bendición de las Flores las 
+Frutas y el Pan, el Desfile de la Confraternidad y La Ronda Nocturnal.<br><br>
+<em><a href="https://www.patrimoniocultural.gob.ec/fiesta-de-la-fruta-y-de-las-flores/#:~:text=Tungurahua%2C%20Ambato&text=Se%20lleva%20a%20cabo%20entre,Confraternidad%20y%20La%20Ronda%20Nocturnal.">Para mas informacion de click aqui</a></em>
+</p>
+
+</div>
+<div id="dos">
+<h2 id="Sol_Noviembre">Sol de Noviembre</h2>
+<img src="pictures/Sol.PNG" class="dos_img">
+<p>
+Ambato celebra la gesta libertaria del 12 de noviembre de 1820 con un 
+mes de manifestaciones culturales, artísticas y musicales que se denomina 
+‘Sol de Noviembre’.<br><br>
+<em><a href="https://www.turismo.gob.ec/ambato-te-invita-a-disfrutar-sus-festividades-del-sol-de-noviembre/">Para mas informacion de click aqui</a></em>
+</p>
+</div>
+<div id="tres">
+<h2 id="diablada">La diablada pillareña</h2>
+<img src="pictures/diabladaaldianews.jpg" class="uno_img">
+<p>
+El cantón Píllaro revive la mágica tradición ancestral de la diablada, 
+que reúne a una gran número de turistas de distintas partes del Ecuador 
+y el extranjero para disfrutar de baile, colorido e increíbles máscaras 
+típicas de este evento cultural.<br><br>
+<em><a href="
+https://www.goraymi.com/es-ec/tungurahua/pillaro/fiestas-tradicionales/diablada-pillaro-ab8988182#t7vuv8yv">Para mas informacion de click aqui</a></em>
+</p>
+</div>
+<div id="cuatro">
+<h2 id="Señor_Terremoto">Señor del Terremoto de Patate</h2>
+<img src="pictures/terremoto.jpg" class="dos_img">
+<p>
+Cada febrero, el pueblo de Patate se alista para celebrar las festividades en 
+honor al Señor del Terremoto, patrono del ‘Valle de la eterna primavera”. 
+Para el efecto, el comité de fiestas y autoridades municipales organizan 
+eventos de carácter social y religioso para fortalecer la fe y devoción de los feligreses.<br><br>
+<em><a href="https://tungurahuaturismo.com/es-ec/tungurahua/patate/fiestas-tradicionales/senor-terremoto-patate-a06d57546">Para mas informacion de click aqui</a></em>
+</p>
+</div>
+    </body>
+</html>
