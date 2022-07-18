64e7111865da5b74e730cd3950c7e38d7726a9c9
mou <mvillafuerte0383@uta.edu.ec>
2022-07-17 20:40:53 -0500

    ubicaciones

--- turismo_tungurahua/views/interfaces/servicios.php
+++ turismo_tungurahua/views/interfaces/servicios.php
@@ -1,1 +1,107 @@
-Estas en Ubicaciones
+<html>
+<head>
+</head>
+<style>
+#Caratula{
+    background-color: #F29F05;
+    font-family: Arial, Helvetica, sans-serif;
+    background-image: url(pictures/LUHUBICACION.png);
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 100%;
+    height: 400px;
+    padding: 0px;
+    
+}
+#Introduccion{
+    font-size: 1.4em;
+    position: absolute;
+        left: 60%;
+       background-size: cover;
+       background-color: #FFFFFF;
+       opacity: .4;
+       height: 400px;
+       padding: 20px;
+       }
+#uno{
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 1.8em;
+       height: 400px;
+       padding: 20px;
+       }
+.uno_img{
+    padding: 20px;
+    width: 40%;
+    float: left;
+    margin-right: 1em;
+}
+.dos_img{
+    padding: 20px;
+    width: 40%;
+    float: left;
+    margin-right: 1em;
+}
+</style> 
+<body>
+<div id="Caratula" >   
+    <div id="Introduccion">
+    <p>
+    Tungurahua es una de las 24 provincias que conforman la República del Ecuador, situada en el centro del país,
+    en la zona geográfica conocida como región interandina o sierra, principalmente sobre la hoya de Patate. 
+    Su capital administrativa es la ciudad de Ambato, la cual además es su urbe más grande y poblada
+    </p> 
+    
+    </div>
+</div>
+<div id="uno">
+    
+
+<img src="pictures/LUPELILEO.png" class="uno_img">
+<h2>AMBATO</h2>
+<p>
+Se encuentra en la Cordillera Occidental, está enclavada en una hondonada formada por seis mesetas: 
+    Píllaro, Quisapincha, Tisaleo, Quero, Huambaló; y Cotaló;, lo que le da un clima agradable, Ambato 
+    está ubicada a 78°; 37' 11’’; de longitud con relación al Meridiano de Greenwich y a 1° 13' 28” de 
+    latitud sur con relación a la Línea Equinoccial, a 2.577 metros sobre el nivel del mar.
+</p>
+</div>
+
+<div id="uno">
+
+<img src="pictures/LUAMBATO.png" class="dos_img">
+<h2>PELILEO</h2>
+<p>
+La ciudad está ubicada en el centro del país, al suroccidente de la Provincia de Tungurahua a 
+25 km de Ambato. Pelileo se sitúa a 2600 metros sobre el nivel del mar. Posee un clima templado y seco.
+ Territorialmente, la ciudad de Pelileo está organizada en dos parroquias urbanas, mientras que existen 
+ ocho parroquias rurales con las que complementa el aérea total del Cantón Pelileo
+</p>
+</div>
+
+<div id="uno">
+
+<img src="pictures/LUPATATE.png" class="uno_img">
+<h2>BAÑOS</h2>
+<p>
+Situado en los flancos externos de la cordillera oriental de los Andes, en las 
+faldas del volcán Tungurahua, a una altitud de 1820 msnm. Se encuentra a 40 km al 
+este de Ambato, en la provincia de Tungurahua Tiene una gran riqueza hidrológica, con 
+algunos ríos en sus cercanías, como el río Bascún (al oeste), el río Ulba 
+(al oriente de la ciudad) y principalmente, el río Pastaza que limita la ciudad al sur. 
+Latitud: 2º 55' S. Longitud: 79º 04' O.
+</p>
+</div>
+
+<div id="uno">
+
+<img src="pictures/LUPATATE.png" class="uno_img">
+<h2>PATATE</h2>
+<p>
+Al norte con el Cantón Píllaro y la provincia de Napo, sur con los cantones 
+Baños y Pelileo, este con el cantón Baños, oeste con cantones Píllaro y Pelileo, 
+La ciudad y el cantón Patate, al igual que las demás localidades ecuatorianas, 
+se rige por una municipalidad según lo previsto en la Constitución de la República.
+</p>
+</div>
+    </body>
+</html>
